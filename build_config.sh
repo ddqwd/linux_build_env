ddc3790241306e6e42d218096d96709c4f43512e
shiji <shiji>
2024-02-26 21:08:42 +0800

    add new config

--- build_config.sh
+++ build_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # install 
-sudo apt install fzf ripgrep eog bat autojump exa git cmake python3-dev g++ 
+sudo apt install fzf ripgrep eog bat autojump exa git cmake python3-dev g++  python3-pip pkg-config libdrm* libelf-dev libglvnd-dev bison wayland* libwayland-dev libwayland-egl* libxext-dev libxshmfence* libxxf86vm* libxrandr* libelf* libxrandr* libxdamage* libsensors* xcb-glx libxcb-glx0 libxcb-glx0-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-present-dev ninja-build
+
 # mesa-18.3.6
 
 
