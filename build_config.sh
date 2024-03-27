762e26625d562ba0748aa9bba7946e7d015d7301
ddqwd <48583008+ddqwd@users.noreply.github.com>
2024-03-27 17:19:23 +0800

    Update build_config.sh

--- build_config.sh
+++ build_config.sh
@@ -2,6 +2,9 @@
 # install 
 sudo apt install fzf ripgrep eog bat autojump exa git cmake python3-dev g++  python3-pip pkg-config libdrm* libelf-dev libglvnd-dev bison wayland* libwayland-dev libwayland-egl* libxext-dev libxshmfence* libxxf86vm* libxrandr* libelf* libxrandr* libxdamage* libsensors* xcb-glx libxcb-glx0 libxcb-glx0-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-present-dev ninja-build
 
+sudo apt install pkg-config
+sudo apt install meson
+
 # mesa-18.3.6
 
 
